9f5dfef2fa3cf3dd791ac4d110caad72ba820719
2tzz <157218559+2tzz@users.noreply.github.com>
2025-05-01 15:09:07 +0530

    done until cart

--- apiApp/serializers.py
+++ apiApp/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Product , Category
+from .models import CartItem, Product , Category , Cart
 
 class ProductListSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,12 +10,63 @@ class ProductListSerializer(serializers.ModelSerializer):
 class ProductDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id' , 'name' ,'description' ,  'slug' , 'description' , 'image' , 'price']
+        fields = ['id' , 'name' ,  'slug' ,'image' ]
 
 
-class CategorySerializer(serializers.ModelSerializer):
-    prodacts = ProductListSerializer(many=True , read_only=True)
+class CategoryListSerializer(serializers.ModelSerializer):
+    products = ProductListSerializer(many=True, read_only=True)
+
     class Meta:
         model = Category
-        fields = ['id' , 'name' , 'image' , 'products']
+        fields = ['id', 'name', 'image', 'products']
+
+class CategoryDetailSerializer(serializers.ModelSerializer):
+    products = ProductListSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'image', 'products']
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    product = ProductListSerializer(read_only=True)
+    sub_total = serializers.SerializerMethodField()
+    class Meta :
+        model= CartItem
+        fields = ['id' , 'product' , 'quantity' , 'sub_total']
+
+    def get_sub_total(self , cartitem):
+        total = cartitem.product.price * cartitem.quantity
+        return total
+    
+class CartSerializer(serializers.ModelSerializer):
+    cartitems = CartItemSerializer(read_only=True ,many=True)
+    cart_total = serializers.SerializerMethodField()
+    class Meta:
+        model = Cart
+        fields = ['id' , 'cart_code' , 'cartitems' , 'sub_total' , 'cart_total']
+
+    def get_cart_total(self , cart):
+        items = cart.cartitems.all()
+        total = sum([item.quantity * item.product.price for item in items])
+        return total
+    
+
+class CartStatSerializer(serializers.ModelSerializer):
+    total_quantity = serializers.SerializerMethodField()
+    class Meta:
+        model = Cart
+        fields = ['id' , 'cart_code' , 'total_quantity' , 'sub_total' , 'cart_total']
+
+    def get_total_quantity(self , cart):
+        items = cart.cartitems.all()
+        total = sum([item.quantity for item in items])
+        return total
+
+
+
+
+
+
+
 
--- apiApp/views.py
+++ apiApp/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Product
-from .serializers import ProductListSerializer
+from .models import Cart, CartItem, Product, Category
+from .serializers import CartSerializer, ProductListSerializer , ProductDetailSerializer , CategoryListSerializer , CategoryDetailSerializer 
 
 # Create your views here.
 
@@ -17,3 +17,40 @@ def product_list(request):
 
 @api_view(['GET'])
 def product_detail(request , slug) :
+    prodct = Product.objects.get(slug=slug)
+    serializer = ProductDetailSerializer(prodct)
+    return Response(serializer.data)
+
+
+
+@api_view(['GET'])
+def category_list(request):
+    categories = Category.objects.all()
+    serializer = CategoryListSerializer(categories , many=True)
+    return Response(serializer.data)
+
+
+
+@api_view(['GET'])
+def category_detail(request , slug):
+    categories = Category.objects.get(slug=slug)
+    serializer = CategoryDetailSerializer(categories)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def add_to_cart(request):
+    cart_code = request.data.get('cart_code')
+    product_id = request.data.get('product_id')
+
+    cart, created = Cart.objects.get_or_create(cart_code=cart_code)
+    product = Product.objects.get(id=product_id)
+
+
+    cartitem,created = CartItem.objects.get_or_create(product=product , cart=cart)
+    cartitem.quantity = 1
+    cartitem.save()
+
+    serializer = CartSerializer(cart)
+    return Response(serializer.data)
+
